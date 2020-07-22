cb8075c513ce06660fc67d7f6f465dfff678863a
asriidev-gh <asriidev@gmail.com>
2020-07-22 15:17:00 +0800

    apply todo ui and theme

--- view/src/App.js
+++ view/src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import login from './pages/login';
+import signup from './pages/signup';
+import home from './pages/home';
+
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#33c9dc',
+			main: '#FF5722',
+			dark: '#d50000',
+			contrastText: '#fff'
+		}
+	}
+});
 
 function App() {
   return (
     <div className="App">
-      <Router>
-          <div>
-            <Switch>
-                <Route exact path="/login" component={login}/>
-            </Switch>
-          </div>
+      <Router>          
+            <MuiThemeProvider theme={theme}>
+              <Switch>
+                  <Route exact path="/login" component={login}/>
+                  <Route exact path="/signup" component={signup}/>
+                  <Route exact path="/" component={home}/>
+              </Switch>
+            </MuiThemeProvider>                    
       </Router>
     </div>
   );
